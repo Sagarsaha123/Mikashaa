1aecfaf9dbee7d73e84325edca9a0047c47471d4
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-23 00:28:07 +0530

    Update settings.py

--- MikashaaAi/plugins/bot/settings.py
+++ MikashaaAi/plugins/bot/settings.py
@@ -5,9 +5,10 @@ from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
+    InputMediaPhoto,
+    InputMediaVideo,
     Message,
 )
-
 from MikashaaAi import app
 from MikashaaAi.utils.database import (
     add_nonadmin_chat,
@@ -34,8 +35,7 @@ from MikashaaAi.utils.inline.settings import (
     vote_mode_markup,
 )
 from MikashaaAi.utils.inline.start import private_panel
-from config import BANNED_USERS, OWNER_ID
-
+from config import BANNED_USERS, OWNER_ID, CLEANMODE_DELETE_MINS, MUSIC_BOT_NAME
 
 @app.on_message(
     filters.command(["settings", "setting"]) & filters.group & ~BANNED_USERS

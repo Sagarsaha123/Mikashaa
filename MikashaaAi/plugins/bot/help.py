5943d12dc9b1cb6c616faf305574e8538a47fc6a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-23 01:04:32 +0530

    Update help.py

--- MikashaaAi/plugins/bot/help.py
+++ MikashaaAi/plugins/bot/help.py
@@ -8,7 +8,7 @@ from MikashaaAi.utils import help_pannel
 from MikashaaAi.utils.database import get_lang
 from MikashaaAi.utils.decorators.language import LanguageStart, languageCB
 from MikashaaAi.utils.inline.help import help_back_markup, private_help_panel
-from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
+from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT, AM_CHAT
 from strings import get_string, helpers
 
 
@@ -28,7 +28,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         await update.edit_message_text(
-            _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
+            _["help_1"].format(AM_CHAT), reply_markup=keyboard
         )
     else:
         try:
@@ -40,7 +40,7 @@ async def helper_private(
         keyboard = help_pannel(_)
         await update.reply_photo(
             photo=START_IMG_URL,
-            caption=_["help_1"].format(SUPPORT_CHAT),
+            caption=_["help_1"].format(AM_CHAT),
             reply_markup=keyboard,
         )
 

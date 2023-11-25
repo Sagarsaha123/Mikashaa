b58474bb8ecd49e16121738bd6bbc93230e79c11
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-25 23:23:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","12227067"))
-API_HASH = getenv("API_HASH","b463bedd791aa733ae2297e6520302fe")
+API_ID = int(getenv("API_ID","6435225"))
+API_HASH = getenv("API_HASH","4e984ea35f854762dcde906dce426c2d")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_USERNAME = getenv("BOT_USERNAME","MikaShaaAIBot")
 USERNAME = getenv("USERNAME","MikaShaaAIBot")

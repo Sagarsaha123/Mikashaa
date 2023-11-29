d2e9382cc07283d47495d4ffd561271135e0e415
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-30 02:48:27 +0530

    Update logger.py

--- MikashaaAi/utils/logger.py
+++ MikashaaAi/utils/logger.py
@@ -9,8 +9,8 @@ async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-<b>𝘾𝙝𝙖𝙩 𝙉𝙖𝙢𝙚 ➪ </b> {message.chat.title}
-<b>𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> `{message.chat.id}`
+<b>◈ 𝘾𝙝𝙖𝙩 𝙉𝙖𝙢𝙚 ➪ </b> {message.chat.title}
+<b>◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> `{message.chat.id}`
 <b>◈ 𝐔𝐬𝐞𝐫 ➪ </b> {message.from_user.mention}
 <b>◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ </b> @{message.from_user.username}
 <b>◈ 𝐈𝐝 ➪ </b> <code>{message.from_user.id}</code>

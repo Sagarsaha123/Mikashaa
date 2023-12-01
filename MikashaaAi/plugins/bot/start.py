18ba5f716a69710343c090681c987f07ece8c3a5
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-01 11:04:57 +0530

    Update start.py

--- MikashaaAi/plugins/bot/start.py
+++ MikashaaAi/plugins/bot/start.py
@@ -92,7 +92,7 @@ async def start_pm(client, message: Message, _):
                     chat_id=config.LOGGER_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-   else:
+else:
     out = private_panel(_)
     UP, CPU, RAM, DISK = await bot_sys_stats()
     await message.reply_photo(

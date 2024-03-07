079e24c1581a41cc5cb2ea5ec620396a91276934
Sagarsaha123 <144699243+Sagarsaha123@users.noreply.github.com>
2024-03-07 22:41:37 +0530

    Update gpdata.py

--- MikashaaAi/plugins/tools/gpdata.py
+++ MikashaaAi/plugins/tools/gpdata.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from MikashaaAi import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from MikashaaAi.utils.daxx_ban import admin_filter
+from MikashaaAi.utils.mikasha_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 

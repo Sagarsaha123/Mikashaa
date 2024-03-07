35107de67daf12600f9e2d34df4f85cca794b3e2
Satyambots72 <152467690+Satyambots72@users.noreply.github.com>
2024-03-07 20:51:37 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf Mikashaa_AI && git clone https://github.com/AMOPBOT/Mikashaa_AI && cd Mikashaa_AI && python -m MikashaaAi")
+system ("cd && rm -rf Mikashaa_AI && git clone https://github.com//Mikashaa_AI && cd Mikashaa_AI && python -m MikashaaAi")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")

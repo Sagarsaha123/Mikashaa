b72c6f563cbfefd64081a3fef77663bbdf4ec517
Sagarsaha123 <144699243+Sagarsaha123@users.noreply.github.com>
2024-03-07 22:33:33 +0530

    Update purge.py

--- MikashaaAi/plugins/admins/purge.py
+++ MikashaaAi/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from MikashaaAi.utils.daxx_ban import admin_filter
+from MikashaaAi.utils.mikasha_ban import admin_filter
 from MikashaaAi import app
 
 
